bf2a5f3e8350cd8044853a644d5850d14dc023ce
ahalrasheed <ahmed.alrasheed88@gmail.com>
2019-07-30 18:40:40 +0300

    done with the stupid error

--- task_1/settings.py
+++ task_1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'flights',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
